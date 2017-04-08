fd8bed25bfb2e4dadd8d1a34a170d7a011676575
voliseq <maciej.radziszowski@gmail.com>
2017-04-08 15:50:15 +0200

    Templapte jquery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 (function(){
 
-
-    var myButton = document.getElementById("colorChange");
-    myButton.addEventListener('click', function(e){
-        e = e || window.event;
-        var target = e.target;
-        if(target.style.backgroundColor == "yellow"){
-            target.style.backgroundColor = "blue"
-        }else{
-            target.style.backgroundColor = "yellow";
-        }
-    })
-
-
+$("#content").load("templates/home.html");
 
 })();
