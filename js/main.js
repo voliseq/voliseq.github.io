de4a0f1dbbe254beb4a22ebf7fb4085464853777
voliseq <maciej.radziszowski@gmail.com>
2017-04-14 03:33:14 +0200

    nav link fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (function(){
 
-$("#content").load("templates/portfolio.html");
+// $("#content").load("templates/portfolio.html");
 
 
-$('a').click(function(e){
+$('.box a').click(function(e){
+    console.log("aa");
     e.preventDefault();
     var template = $(this).attr('href');
     $("#content").load(template);
