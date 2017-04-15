642af42bb9bf4d8b8cca374eb99307dd5f0dd451
voliseq <maciej.radziszowski@gmail.com>
2017-04-15 19:48:09 +0200

    addes texture

--- js/main.js
+++ js/main.js
@@ -3,11 +3,27 @@
 // $("#content").load("templates/portfolio.html");
 
 
-    $('.box a, .technology a').bind('click', function (e) {
-        console.log("aa");
+    $('header .box a').bind('click', function (e) {
         e.preventDefault();
+        $("#content").finish();
         var template = $(this).attr('href');
-        $("#content").load(template);
+        $("#content").fadeTo(0, 0);
+        $("#content").load(template, function(){
+            $("#content").fadeTo('slow', 1);
+        });
+
+    })
+
+    $('footer .box a').bind('click', function (e) {
+        e.preventDefault();
+        $("#content").finish();
+        var template = $(this).attr('href');
+        $("#content").fadeTo(0, 0);
+        $("#content").load(template, function(){
+            $("#content").fadeTo('slow', 1);
+            $("html, body").animate({scrollTop: 0}, 800);
+        });
+
     })
 
 })();
